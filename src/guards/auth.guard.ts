df76569d365fd434febdf669926bae182d8e8142
LowkyVu <cuongmc1404@gmail.com>
2024-05-13 17:28:10 +0700

    Authentication From Scratch

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -8,10 +8,7 @@ import {
 export class AuthGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
-    // const user = request.session.user;
-    // if (!user) {
-    //   throw new UnauthorizedException();
-    // }
+
     return request.session.userId;
   }
 }
